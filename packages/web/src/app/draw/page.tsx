fda3664eab21eafcf0950fae1a5ad403f5c1b87c
Bernardo <blvieira5@gmail.com>
2026-09-24 10:56:06 +0100

    fix(web): remove stale draw lint suppression

--- packages/web/src/app/draw/page.tsx
+++ packages/web/src/app/draw/page.tsx
@@ -220,7 +220,6 @@ export default function DrawPage() {
   useEffect(() => {
     if (!session?.address) return;
     void refresh();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session?.address]);
 
   function activeSession() {
