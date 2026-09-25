8b2f22ebefed58061d9ba7c365a261a1402aa75e
Bernardo <blvieira5@gmail.com>
2026-09-25 11:22:38 +0100

    fix: align Netlify with static export

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   transpilePackages: ["@sortecerta/protocol", "@web3auth/modal"],
+  generateBuildId: async () => "static",
   images: {
     unoptimized: true,
     remotePatterns: [{ protocol: "https", hostname: "**" }],
