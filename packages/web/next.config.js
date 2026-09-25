64c26dd148cb7b0d737f88d181130b2992da9bb0
Bernardo <blvieira5@gmail.com>
2026-09-25 10:49:28 +0100

    fix: configure Cloudflare Workers deploy

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,22 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   transpilePackages: ["@sortecerta/protocol", "@web3auth/modal"],
   images: {
+    unoptimized: true,
     remotePatterns: [{ protocol: "https", hostname: "**" }],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-        ],
-      },
-    ];
   },
 };
 
